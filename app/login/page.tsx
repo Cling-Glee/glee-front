a5a3c790862b872b5193c68be1af61396f8fa025
DaegyunOh <oooppq@naver.com>
2023-08-17 14:45:20 +0900

    refactor: redirect uri 관리방식 변경
    .env.development와 .env.production을 생성하여 환경변수로 관리하도록 변경했다.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,8 +8,8 @@ const Login = () => {
   const handleClickKakaoLoginButton = () => {};
   const handleClickTwitterLoginButton = () => {
     const key = process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID;
-    const redirectUri = 'http://localhost:3000';
-    const authUrl = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${key}&redirect_uri=${redirectUri}/login&scope=tweet.read%20users.read%20offline.access&state=state&code_challenge=challenge&code_challenge_method=plain`;
+    const redirectUri = process.env.NEXT_PUBLIC_SOCIAL_LOGIN_REDIRECT_URI;
+    const authUrl = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${key}&redirect_uri=${redirectUri}&scope=tweet.read%20users.read%20offline.access&state=state&code_challenge=challenge&code_challenge_method=plain`;
     window.location.href = authUrl;
   };
   const handleClickInstagramLoginButton = () => {};
